8f532889c2c586d98281abf8cf1ad45918870c46
uaparicio <vpnulises@city-furniture.com>
2022-04-25 00:55:19 -0300

    configuring allure report

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,9 @@
 require "selenium-webdriver"
-require 'page-object'
+require "allure-cucumber"
 
-World(PageObject::PageFactory)
+AllureCucumber.configure do |config|
+    config.results_directory = "report/allure-results"
+    config.clean_results_directory = true
+    config.logging_level = Logger::INFO
+    config.logger = Logger.new($stdout, Logger::INFO)
+end
